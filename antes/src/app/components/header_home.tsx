1e48f9caa6db7ae6ba52252e3ac975ba2d1d6b6d
Albadit <46565849+Albadit@users.noreply.github.com>
2023-10-06 13:27:13 +0200

    done

--- antes/src/app/components/header_home.tsx
+++ antes/src/app/components/header_home.tsx
@@ -3,25 +3,24 @@
 import { useState } from 'react'
 
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Home', href: '#' },
+  { name: 'Werken bij ons', href: '#' },
+  { name: 'Over ons ', href: '#' },
+  { name: 'Contact', href: '#' },
 ]
 
 export default function HeaderHome() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-background">
+    <header className="bg-background shadow-lg">
       <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
             <img
               className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt=""
+              src="antes_logo.png"
+              alt="antes logo"
             />
           </a>
         </div>
@@ -31,19 +30,19 @@ export default function HeaderHome() {
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
             onClick={() => setMobileMenuOpen(true)}
           >
-            <span className="sr-only">Open main menu</span>
+            <p>open</p>
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+            <a key={item.name} href={item.href} className="text-base font-font1 leading-6 text-font1">
               {item.name}
             </a>
           ))}
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
+          <a href="#" className="text-base font-font1 font-medium leading-6">
+            Inloggen <span className='font-semibold' aria-hidden="true">&rarr;</span>
           </a>
         </div>
       </nav>
