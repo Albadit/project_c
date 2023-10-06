cf12c571c61ac7615f6d02d9edd047b197b2377c
Albadit <46565849+Albadit@users.noreply.github.com>
2023-10-06 20:26:11 +0200

    clean

---
+++ antes/src/app/components/icon/hambuger_menu.tsx
@@ -0,0 +1,9 @@
+import React, { SVGProps } from 'react'
+
+export default function IconHambugerMenu(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className='fill-font1' viewBox="0 0 24 24" {...props}>
+      <path d="M4 18q-.425 0-.713-.288T3 17q0-.425.288-.713T4 16h16q.425 0 .713.288T21 17q0 .425-.288.713T20 18H4Zm0-5q-.425 0-.713-.288T3 12q0-.425.288-.713T4 11h16q.425 0 .713.288T21 12q0 .425-.288.713T20 13H4Zm0-5q-.425 0-.713-.288T3 7q0-.425.288-.713T4 6h16q.425 0 .713.288T21 7q0 .425-.288.713T20 8H4Z"></path>
+    </svg>
+  )
+}
