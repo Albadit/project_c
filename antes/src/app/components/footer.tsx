5d0efa7122077953bfff7c621caa08d472a55d73
Albadit <46565849+Albadit@users.noreply.github.com>
2023-10-09 17:52:44 +0200

    footer done

--- antes/src/app/components/footer.tsx
+++ antes/src/app/components/footer.tsx
@@ -10,30 +10,28 @@ const navigation = [
   { name: 'Contact', href: '#' },
 ]
 
+const iconSize = "[22px]"
 const socialMedia = [
-  { name: 'Facebook', icon: <Facebook className="h-full"/>, href: '#' },
-  { name: 'Linkedin', icon: <Linkedin className="h-full"/>, href: '#' },
-  { name: 'Twitter', icon: <Twitter className="h-full"/>, href: '#' },
-  { name: 'Youtube', icon: <Youtube className="h-full"/>, href: '#' },
-  { name: 'Instagram', icon: <Instagram className="h-full"/>, href: '#' },
+  { name: 'Facebook', icon: <Facebook className={`h-${iconSize}`}/>, href: '#' },
+  { name: 'Linkedin', icon: <Linkedin className={`h-${iconSize}`}/>, href: '#' },
+  { name: 'Twitter', icon: <Twitter className={`h-${iconSize}`}/>, href: '#' },
+  { name: 'Youtube', icon: <Youtube className={`h-${iconSize}`}/>, href: '#' },
+  { name: 'Instagram', icon: <Instagram className={`h-${iconSize}`}/>, href: '#' },
 ]
 
-export default function Footer() {
-  const year = (new Date().getFullYear())
+const year = (new Date().getFullYear())
 
+export default function Footer() {
   return (
     <footer className='flex justify-center justify-items-center bg:background shadow-cbs'>
       <nav className='flex flex-col gap-5 p-5 max-w-[1040px] w-full'>
-        <div>
-          <p className='text-extra'>&copy;{year} Antes</p>
-          
-        </div>
-        <hr className='text-extra'/>
+        <p className='text-extra'>&copy;{year} Antes</p>
+        <hr className='text-extra/50'/>
         <div className='flex flex-col sm:flex-row gap-5 justify-between'>
           <ul className='flex gap-x-10'>
           {navigation.map((item) => (
-            <li>
-              <a key={item.name} href={item.href} className="text-base font-font1 leading-6 text-extra">
+            <li key={item.name}>
+              <a href={item.href} className="text-base font-font1 text-extra">
                 {item.name}
               </a>
             </li>
@@ -41,8 +39,8 @@ export default function Footer() {
           </ul>
           <ul className='flex items-center gap-x-4'>
           {socialMedia.map((item) => (
-            <li>
-              <a key={item.name} href={item.href} className="h-[22px] text-base font-font1 leading-6 fill-extra">
+            <li key={item.name}>
+              <a href={item.href} aria-label={item.name} className="text-base font-font1 fill-extra">
                 {item.icon}
               </a>
             </li>
--- antes/src/app/components/header_home.tsx
+++ antes/src/app/components/header_home.tsx
@@ -17,67 +17,68 @@ export default function HeaderHome() {
 
   return (
     <header className="bg-background shadow-cbs">
-      <nav className="flex items-center justify-between p-5 lg:px-8" aria-label="Global">
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <img
-              className="h-8 w-auto"
-              src="antes_logo.png"
-              alt="antes logo"
-            />
-          </a>
-        </div>
+      <nav className="flex items-center justify-between p-5 lg:px-8" >
+        {/* Desktop */}
+        <ul>
+          <li className="flex lg:flex-1">
+            <a href="#" aria-label="logo" className="-m-1.5 p-1.5">
+              <img src="antes_logo.png" alt="antes logo" className="h-8 w-auto"/>
+            </a>
+          </li>
+        </ul>
+        <ul className='hidden lg:flex gap-x-12'>
+        {navigation.map((item) => (
+          <li key={item.name}>
+            <a href={item.href} className="text-base font-font1 leading-6 text-font1">
+              {item.name}
+            </a>
+          </li>
+        ))}
+        </ul>
+        <ul className="hidden lg:flex">
+          <li>
+            <a href="#" className="flex items-center text-base font-font1 font-medium leading-6">
+              Inloggen <IconArrowForwardRounded className="w-4 ml-1"/>
+            </a>
+          </li>
+        </ul>
         <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          <button type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
             onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
           >
             <IconHamburgerMenu  className="w-8"/>
           </button>
         </div>
-        <div className="hidden lg:flex lg:gap-x-12">
-          {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-base font-font1 leading-6 text-font1">
-              {item.name}
-            </a>
-          ))}
-        </div>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="flex items-center text-base font-font1 font-medium leading-6">
-            Inloggen <IconArrowForwardRounded className="w-4 ml-1"/>
-          </a>
-        </div>
+
+        {/* Mobile */}
         <div className={`lg:hidden flex flex-col absolute top-0 right-0 h-full w-full sm:w-[300px] outline outline-1 outline-font1/20 bg-background ${mobileMenuOpen ? 'block' : 'hidden'}`}>
           <div className="flex justify-between sm:justify-end p-5">
             <div className="flex sm:hidden">
             <a href="#" className="-m-1.5 p-1.5">
-              <img
-                className="h-8 w-auto"
-                src="antes_logo.png"
-                alt="antes logo"
-              />
+              <img src="antes_logo.png" alt="antes logo" className="h-8 w-auto"/>
             </a>
           </div>
-            <button
-              type="button"
+            <button type="button"
               className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
               onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
             >
-              <IconClose className='w-7 '/>
+              <IconClose className='w-7'/>
             </button>
           </div>
-          <div className="flex flex-col gap-y-8 p-5">
-            {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-base font-font1 leading-6 text-font1">
+          <ul className="flex flex-col gap-y-8 p-5">
+          {navigation.map((item) => (
+            <li key={item.name}>
+              <a href={item.href} className="text-base font-font1 leading-6 text-font1">
                 {item.name}
               </a>
-            ))}
+            </li>
+          ))}
             <hr className='border-font1/20'/>
             <a href="#" className="flex items-center text-base font-font1 font-medium leading-6">
               Inloggen
             </a>
-          </div>
+          </ul>
         </div>
       </nav>
     </header>
