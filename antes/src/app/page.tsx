154dbf9b3d7d3afc7f8d0c60235015aa5cabf49d
Ceydag <100134625+Ceydag@users.noreply.github.com>
2023-10-10 19:30:03 +0200

    eventcard toegevoegd

--- antes/src/app/page.tsx
+++ antes/src/app/page.tsx
@@ -1,10 +1,17 @@
 import Image from 'next/image'
 import HeaderHome from '@/app/components/header_home'
+import { EventCard } from '@/app/components/EventCard';
 
 export default function Home() {
   return (
     <>
-      <HeaderHome/>
+      <HeaderHome />
+      <EventCard
+        title="Event: Connectiedag!"
+        date="2023-30-01"
+        location="Meld je aan voor de connectiedag"
+        imageSrc="event.png"
+      />
     </>
   )
 }
