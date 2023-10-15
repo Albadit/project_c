e6b02de66e07e3ef9cbf76a954896eb5f81f9edd
Albadit <46565849+Albadit@users.noreply.github.com>
2023-10-15 21:13:19 +0200

    Update page.tsx

--- antes/src/app/dashboard/page.tsx
+++ antes/src/app/dashboard/page.tsx
@@ -8,6 +8,10 @@ export default function Dashboard() {
       <header>
         <NavDashboard user={{}}/>
       </header>
+      <main className='flex flex-col gap-5 '>
+        <p>gemaakt door ayoeb geinspireerd door hicham</p>
+        <a href="/" className='bg-[#AE0800] hover:bg-[#000] hover:text-[#fff] text-white rounded-[10px] py-1 px-5 text-xl'>tests</a>
+      </main>
       {/* <Footer/> */}
     </>
   )
