10c31c1fb9362393d1f791df11a96e6ff7b4d1a7
TheRapael <1038660@hr.nl>
2023-11-15 15:07:24 +0100

    agenda page gemaakt
    ik heb een agenda page gemaakt

---
+++ antes/src/app/agenda/page.tsx
@@ -0,0 +1,124 @@
+import HeaderHome from '@/app/components/header_home'
+
+export default function Home() {
+  // Fixed time slots and grid lines
+  const timeSlots = ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"];
+  const dayLabels = ["Ma", "Di", "Wo", "Do", "Vr", "Za", "Zo"];
+
+  const timeSlotHeight = 100 / timeSlots.length; 
+
+  const currentDate = new Date();
+  const currentMonth = currentDate.toLocaleString('default', { month: 'long' })
+  const currentYear = currentDate.getFullYear()
+  const startDayOfWeek = getStartDayOfWeek()
+
+  return (
+    <div className="">
+      {/* Header */}
+      <HeaderHome />
+
+      {/* Title */}
+      <div className="text-center py-4 text-5xl font-bold text-primary">
+        Agenda
+      </div>
+
+      {/* Month and Week Selector */}
+      <div className="flex justify-between items-center px-6 py-2">
+        <div className="text-3xl font-bold">{`${currentMonth} ${currentYear}`}</div>
+        <button className="bg-[#3939bf] font-bold text py-2 px-4 text-[#ffffff] rounded">← Week →</button>
+      </div>
+
+      {/* Calendar Box */}
+      <div className="grid gap-1 shadow-md mx-6 mb-6 p-4 rounded-md">
+        {/* Empty top-left corner */}
+        <div className=""></div>
+
+        {/* Days of the week */}
+        <div className="col-span-7 flex py-2 px-4 rounded-md">
+          {dayLabels.map((day, index) => (
+            <div key={`day-label-${index}`} className="flex-1 text-center">
+              {day} {getUpdatedDayNumber(index, startDayOfWeek)}
+            </div>
+          ))}
+        </div>
+
+        {/* Time slots */}
+        <div className="flex flex-col gap-8">
+          {timeSlots.map((time, index) => (
+            <div key={`time-slot-${index}`} className="text-center" style={{ height: `${timeSlotHeight}%` }}>
+              {time}
+            </div>
+          ))}
+        </div>
+
+        {/* Agenda cells */}
+        <div className="col-start-2 row-start-2 col-span-7 row-span-6 relative">
+          {/* Grid lines for each hour */}
+          {Array.from({ length: 10 }, (_, index) => (
+            <div
+              key={`grid-hour-${index}`}
+              className="absolute border-t"
+              style={{ top: `calc(${index * timeSlotHeight}%)`, width: '100%' }}
+            ></div>
+          ))}
+
+          {/* Grid lines for each day */}
+          {Array.from({ length: 7 }, (_, index) => (
+            <div
+              key={`grid-day-${index}`}
+              className="absolute border-l"
+              style={{ left: `calc(${index * (100 / 7)}%)`, height: '100%' }}
+            ></div>
+          ))}
+
+          {/* Event cards */}
+          {generateEventCard("Di", "12:00", "13:00", "Event: Hans")}
+          {generateEventCard("Za", "09:00", "14:00", "Event: Hans")}
+          {generateEventCard("Ma", "15:00", "17:00", "Event: Hans")}
+          {generateEventCard("Di", "10:00", "11:30", "Event: Hans")}
+          {/* You can add more event cards here */}
+        </div>
+      </div>
+    </div>
+  );
+
+  function convertTimeToMinutes(time) {
+    const [hours, minutes] = time.split(":").map(Number);
+    return hours * 60 + minutes;
+  }
+
+  function getDayIndex(day) {
+    const daysOfWeek = ["Ma", "Di", "Wo", "Do", "Vr", "Za", "Zo"];
+    return daysOfWeek.indexOf(day);
+  }
+
+  function getStartDayOfWeek() {
+    const daysOfWeek = ["Ma", "Di", "Wo", "Do", "Vr", "Za"];
+    return daysOfWeek.indexOf(dayLabels[0]);
+  }
+
+  function getUpdatedDayNumber(index, startDayOfWeek) {
+    const dayNumber = currentDate.getDate() + index - startDayOfWeek - 2;
+    return dayNumber > 0 ? dayNumber : "";
+  }
+
+  function generateEventCard(day, startTime, endTime, eventName) {
+    const startMinutes = convertTimeToMinutes(startTime);
+    const endMinutes = convertTimeToMinutes(endTime);
+    const top = `calc(${startMinutes / 60 - 8} * ${timeSlotHeight}%)`;
+    const height = `calc(${(endMinutes - startMinutes) / 60 * timeSlotHeight}%)`;
+    const left = `calc(${getDayIndex(day) * (100 / 7)}%)`; 
+    const width = 'calc(100% / 7)';
+
+    return (
+      <div
+        key={`${day}-${startTime}`}
+        className="bg-[#3939bf] text-[#ffffff] p-2 rounded-md mb-2 absolute"
+        style={{ top, height, left, width }}
+      >
+        <div className="font-bold">{eventName}</div>
+        <div className="text-sm">{` ${startTime} - ${endTime}`}</div>
+      </div>
+    );
+  }
+}
