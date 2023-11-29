7a025bb088fd6fd205465c220082d1a94e8bda3a
Albadit <ardit-nl@outlook.com>
2023-11-29 12:25:39 +0100

    prisma

--- antes/run.py
+++ antes/run.py
@@ -5,12 +5,13 @@ import os
 script_directory = os.path.dirname(os.path.realpath(__file__))
 install_commands = [
   "npm install next@latest react@latest react-dom@latest",
-  # "npm install next@13.5.4 react@18 react-dom@18",
   "npm install tailwindcss@latest",
   "npm install @tailwindcss/typography",
   "npm install @tailwindcss/forms",
   "npm install @tailwindcss/aspect-ratio",
   "npm install @tailwindcss/container-queries",
+  "npm install prisma --save-dev",
+  "npm install @prisma/client",
 ]
 
 os.chdir(script_directory)
