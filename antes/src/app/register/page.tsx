eeffa322d3a44f1535bf4b5422c833ac57677a51
Mathieu <1053249@hr.nl>
2024-01-11 13:55:56 +0100

    Admin functies toegevoegd

--- antes/src/app/register/page.tsx
+++ antes/src/app/register/page.tsx
@@ -12,7 +12,7 @@ type UserFuntionItems = {
 }
 
 const context = {
-  logo: { url: "/", img: "/img/antes_logo.png", alt: "antes logo"},
+  logo: { url: "/", img: "/img/antes_logo.png", alt: "antes logo" },
   btn: { text: "Registreer" },
   register: { url: "/terms" }
 }
@@ -35,7 +35,7 @@ async function Post(data: any) {
 
 export default function Login() {
   const [message, setMessage] = useState('');
-  const router = useRouter(); 
+  const router = useRouter();
 
   const [userFunctionData, setData] = useState<UserFuntionItems[]>([]);
   const [isLoading, setLoading] = useState(true)
@@ -59,7 +59,7 @@ export default function Login() {
     const password = formData.get('password');
     const confirmPassword = formData.get('confirm_password');
     const agree = formData.get('agree');
-    
+
     if (firstName && lastName && userFunction && email && password && confirmPassword) {
       const register = await Post({
         firstName: firstName,
@@ -85,25 +85,25 @@ export default function Login() {
       <div className="bg-login bg-center grow bg-cover bg-no-repeat md:block hidden"></div>
       <div className='flex flex-col gap-12 justify-center w-full max-w-[30.625rem] p-5 md:p-12'>
         {!isLoading ? (
-        <>
-          <Link href={context.logo.url}>
-          <img src={context.logo.img} alt={context.logo.alt} className='w-max'/>
-          </Link>
-          <form onSubmit={handleSubmit} className='flex flex-col justify-center gap-5'>
-            <Input label="Voornaam" name="first_name" type="text" value=''/>
-            <Input label="Achternaam" name="last_name" type="text" value=''/>
-            <SelectMenu label="Functie" name="user_function" options={userFunctionData}/>
-            <Input label="Email" name="email" type="email" value=''/>
-            <Input label="Wachtwoord" name="password" type="password" value=''/>
-            <Input label="Bevestigen Wachtwoord" name="confirm_password" type="password" value=''/>
-            <div className='flex flex-row gap-3 items-top text-font1 text-sm'>
-              <input type="checkbox" name="agree" className='rounded text-callToAction border-font1/30 focus:ring-2 focus:ring-callToAction sm:text-sm sm:leading-6 disabled:bg-slate-50 disabled:text-text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:opacity-75'/> 
-              <label className='m-[-1px]'>Ga akkoord met de <Link href={context.register.url} className='underline underline-offset-1 text-hyperlink'>Antes-gebruikersovereenkomst en het privacybeleid</Link></label>
-            </div>
-            <button type="submit" title="register" className='flex flex-row items-center justify-center w-full lg:w-auto gap-2 px-4 py-3 rounded-lg bg-primary text-font2 font-semibold text-base'>{context.btn.text}</button>
-            <p className='text-error'>{message}</p>
-          </form>
-        </>
+          <>
+            <Link href={context.logo.url}>
+              <img src={context.logo.img} alt={context.logo.alt} className='w-max' />
+            </Link>
+            <form onSubmit={handleSubmit} className='flex flex-col justify-center gap-5'>
+              <Input label="Voornaam" name="first_name" type="text" value='' />
+              <Input label="Achternaam" name="last_name" type="text" value='' />
+              <SelectMenu label="Functie" name="user_function" options={userFunctionData} />
+              <Input label="Email" name="email" type="email" value='' />
+              <Input label="Wachtwoord" name="password" type="password" value='' />
+              <Input label="Bevestigen Wachtwoord" name="confirm_password" type="password" value='' />
+              <div className='flex flex-row gap-3 items-top text-font1 text-sm'>
+                <input type="checkbox" name="agree" className='rounded text-callToAction border-font1/30 focus:ring-2 focus:ring-callToAction sm:text-sm sm:leading-6 disabled:bg-slate-50 disabled:text-text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:opacity-75' />
+                <label className='m-[-1px]'>Ga akkoord met de <Link href={context.register.url} className='underline underline-offset-1 text-hyperlink'>Antes-gebruikersovereenkomst en het privacybeleid</Link></label>
+              </div>
+              <button type="submit" title="register" className='flex flex-row items-center justify-center w-full lg:w-auto gap-2 px-4 py-3 rounded-lg bg-primary text-font2 font-semibold text-base'>{context.btn.text}</button>
+              <p className='text-error'>{message}</p>
+            </form>
+          </>
         ) : (
           <div className='flex flex-col justify-center items-center gap-6 w-full'>
             <p className="text-xl font-semibold">Loading data...</p>
