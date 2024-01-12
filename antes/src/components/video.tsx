84b6c4ca24a953689e67f04cbca85c065a40fa47
Albadit <46565849+Albadit@users.noreply.github.com>
2024-01-12 12:53:32 +0100

    done

--- antes/src/components/video.tsx
+++ antes/src/components/video.tsx
@@ -1,28 +1,3 @@
-<<<<<<< HEAD:antes/src/app/components/video.tsx
-import React from "react";
-
-interface YouTubeEmbedProps {
-    embedId: string;
-}
-
-
-const video: React.FC<YouTubeEmbedProps> = ({ embedId }) => {
-    return (
-        <div className="aspect-w-16 aspect-h-9 shadow-2xl">
-            <iframe
-                className="w-full h-full"
-                src={`https://www.youtube.com/embed/${embedId}`}
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-            ></iframe>
-        </div>
-    );
-};
-
-export default video;
-=======
 type YouTubeEmbedProps = {
   embedId: string
 }
@@ -39,4 +14,3 @@ export const Video = ({ embedId }: YouTubeEmbedProps) => {
     </div>
   )
 }
->>>>>>> clean-up:antes/src/components/video.tsx
