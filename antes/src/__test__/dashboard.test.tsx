507bd3d6b497db1efb62575412fcc878871bcdab
Mathieu <1053249@hr.nl>
2024-01-17 18:35:19 +0100

    Update dashboard.test.tsx

--- antes/src/__test__/dashboard.test.tsx
+++ antes/src/__test__/dashboard.test.tsx
@@ -1,34 +1,68 @@
-import { render, fireEvent, screen } from '@testing-library/react'
-import { useSession } from 'next-auth/react'
-import Dashboard from '@/app/dashboard/page'
+import { render, fireEvent, screen } from '@testing-library/react';
+import { useSession } from 'next-auth/react';
+import Dashboard from '@/app/dashboard/page';
+import { ELearningCard } from '@/components/dashboard/elearning_card';
 
 // Mock next/router and next-auth/react modules
 jest.mock('next/navigation', () => ({
-  useRouter: () => ({
-    push: jest.fn(),
-    route: '/',
-    query: {},
-  }),
-}))
+    useRouter: () => ({
+        push: jest.fn(),
+        route: '/dashboard',
+        query: {},
+    }),
+}));
 
-jest.mock('next-auth/react')
+jest.mock('next-auth/react');
 
 describe('Dashboard', () => {
-  it('redirects correctly when the QA button is pressed', () => {
-    // Mocking the authenticated session
-    (useSession as jest.Mock).mockReturnValue({
-      data: { user: { name: 'John Doe', email: 'johndoe@example.com' }, expires: '2024-01-01T00:00:00.000Z' },
-      status: 'authenticated',
-    })
+    test('redirects correctly when the E-Learning button is pressed', async () => {
+        // Mocking the authenticated session
+        (useSession as jest.Mock).mockReturnValue({
+            data: { user: { id: 'clri1xxm6000a12aqak9h1fre', name: 'Ardit Fazliji', email: 'johndoe@example.com' }, expires: '2024-02-20T00:00:00.000Z' },
+            status: 'authenticated',
+        });
 
-    // Rendering the component
-    render(<Dashboard />)
+        // Render the component
+        render(<Dashboard />);
 
-    // Simulating button click
-    fireEvent.click(screen.getByText('Q & A'))
+        const allButtons = screen.getByText('Ga verder');
+        fireEvent.click(allButtons); // Clicks the first button
+        // Checking if push was called with the expected argument
+        const push = require('next/navigation').useRouter().push;
+        expect(push).toHaveBeenCalledWith('/elearning');
+    });
+    test('redirects correctly when the Q&A button is pressed', async () => {
+        // Mocking the authenticated session
+        (useSession as jest.Mock).mockReturnValue({
+            data: { user: { id: 1, name: 'John Doe', email: 'johndoe@example.com' }, expires: '2024-02-20T00:00:00.000Z' },
+            status: 'authenticated',
+        });
 
-    // Checking if push was called with the expected argument
-    const push = require('next/navigation').useRouter().push
-    expect(push).toHaveBeenCalledWith('/qa')
-  })
-})
+        // Render the component
+        render(<Dashboard />);
+
+        // Simulate button click within ELearningCard component
+        const allButtons = screen.getAllByText('Ga verder');
+        fireEvent.click(allButtons[0]); // Clicks the first button
+        // Checking if push was called with the expected argument
+        const push = require('next/navigation').useRouter().push;
+        expect(push).toHaveBeenCalledWith('/qa');
+    });
+    test('redirects correctly when the event button is pressed', async () => {
+        // Mocking the authenticated session
+        (useSession as jest.Mock).mockReturnValue({
+            data: { user: { id: 1, name: 'John Doe', email: 'johndoe@example.com' }, expires: '2024-02-20T00:00:00.000Z' },
+            status: 'authenticated',
+        });
+
+        // Render the component
+        render(<Dashboard />);
+
+        // Simulate button click within ELearningCard component
+        const allButtons = screen.getAllByText('Ga verder');
+        fireEvent.click(allButtons[1]); // Clicks the first button
+        // Checking if push was called with the expected argument
+        const push = require('next/navigation').useRouter().push;
+        expect(push).toHaveBeenCalledWith('/event');
+    });
+});
