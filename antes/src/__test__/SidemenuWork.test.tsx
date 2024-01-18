bd5ed8984b100478b692c913eafb6db91e5d738d
Albadit <ardit-nl@outlook.com>
2024-01-18 15:40:56 +0100

    done

--- antes/src/__test__/SidemenuWork.test.tsx
+++ antes/src/__test__/SidemenuWork.test.tsx
@@ -1,125 +1,93 @@
-import SideMenuWork from '../app/work/side_menu_work/page';
-import '@testing-library/jest-dom';
-import { render, fireEvent, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import SideMenuWork from '@/app/work/side_menu_work/page'
+import '@testing-library/jest-dom'
+import { render, fireEvent, screen } from '@testing-library/react'
 
 test('clicking on "Werken bij Antes" in WorkWithUs navigates to the correct page', () => {
-    render(
-        <MemoryRouter initialEntries={['/']}>
-            <SideMenuWork />
-        </MemoryRouter>
-    );
-    const linkElement = screen.getByTestId('link-item-werken-bij-antes');
-    const anchorElement = linkElement.querySelector('a');
-    const hrefAttribute = anchorElement?.getAttribute('href');
-    expect(linkElement).toBeInTheDocument();
-    expect(hrefAttribute).toBe('/work');
-});
+  render(<SideMenuWork />)
+  const linkElement = screen.getByTestId('link-item-werken-bij-antes')
+  const anchorElement = linkElement.querySelector('a')
+  const hrefAttribute = anchorElement?.getAttribute('href')
+  expect(linkElement).toBeInTheDocument()
+  expect(hrefAttribute).toBe('/work')
+})
+
 test('clicking on "Vacatures Antes" in WorkWithUs navigates to the correct page', () => {
-    render(
-        <MemoryRouter initialEntries={['/']}>
-            <SideMenuWork />
-        </MemoryRouter>
-    );
-    const linkElement = screen.getByTestId('link-item-vacatures-antes');
-    const anchorElement = linkElement.querySelector('a');
-    const hrefAttribute = anchorElement?.getAttribute('href');
-    expect(linkElement).toBeInTheDocument();
-    expect(hrefAttribute).toBe('https://werkenbijparnassiagroep.nl/vacatures/antes?_ga=2.77328083.776596900.1701802004-1970662864.1694080243');
-});
+  render(<SideMenuWork />)
+  const linkElement = screen.getByTestId('link-item-vacatures-antes')
+  const anchorElement = linkElement.querySelector('a')
+  const hrefAttribute = anchorElement?.getAttribute('href')
+  expect(linkElement).toBeInTheDocument()
+  expect(hrefAttribute).toBe('https://werkenbijparnassiagroep.nl/vacatures/antes?_ga=2.77328083.776596900.1701802004-1970662864.1694080243')
+})
+
 test('clicking on "Solicitatieprocedure" in WorkWithUs navigates to the correct page', () => {
-    render(
-        <MemoryRouter initialEntries={['/']}>
-            <SideMenuWork />
-        </MemoryRouter>
-    );
-    const linkElement = screen.getByTestId('link-item-solicitatieprocedure');
-    const anchorElement = linkElement.querySelector('a');
-    const hrefAttribute = anchorElement?.getAttribute('href');
-    expect(linkElement).toBeInTheDocument();
-    expect(hrefAttribute).toBe('https://werkenbijparnassiagroep.nl/over-ons/sollicitatieprocedure')
-});
+  render(<SideMenuWork />)
+  const linkElement = screen.getByTestId('link-item-solicitatieprocedure')
+  const anchorElement = linkElement.querySelector('a')
+  const hrefAttribute = anchorElement?.getAttribute('href')
+  expect(linkElement).toBeInTheDocument()
+  expect(hrefAttribute).toBe('https://werkenbijparnassiagroep.nl/over-ons/sollicitatieprocedure')
+})
+
 test('clicking on "Arbeidsvoorwaarden" in WorkWithUs navigates to the correct page', () => {
-    render(
-        <MemoryRouter initialEntries={['/']}>
-            <SideMenuWork />
-        </MemoryRouter>
-    );
-    const linkElement = screen.getByTestId('link-item-arbeidsvoorwaarden');
-    const anchorElement = linkElement.querySelector('a');
-    const hrefAttribute = anchorElement?.getAttribute('href');
-    expect(linkElement).toBeInTheDocument();
-    expect(hrefAttribute).toBe('https://werkenbijparnassiagroep.nl/over-ons/jouw-arbeidsvoorwaarden');
-});
+  render(<SideMenuWork />)
+  const linkElement = screen.getByTestId('link-item-arbeidsvoorwaarden')
+  const anchorElement = linkElement.querySelector('a')
+  const hrefAttribute = anchorElement?.getAttribute('href')
+  expect(linkElement).toBeInTheDocument()
+  expect(hrefAttribute).toBe('https://werkenbijparnassiagroep.nl/over-ons/jouw-arbeidsvoorwaarden')
+})
+
 test('clicking on "Flexpool" in WorkWithUs navigates to the correct page', () => {
-    render(
-        <MemoryRouter initialEntries={['/']}>
-            <SideMenuWork />
-        </MemoryRouter>
-    );
-    const linkElement = screen.getByTestId('link-item-flexpool');
-    const anchorElement = linkElement.querySelector('a');
-    const hrefAttribute = anchorElement?.getAttribute('href');
-    expect(linkElement).toBeInTheDocument();
-    expect(hrefAttribute).toBe('/work/Flexpool');
-});
+  render(<SideMenuWork />)
+  const linkElement = screen.getByTestId('link-item-flexpool')
+  const anchorElement = linkElement.querySelector('a')
+  const hrefAttribute = anchorElement?.getAttribute('href')
+  expect(linkElement).toBeInTheDocument()
+  expect(hrefAttribute).toBe('/work/Flexpool')
+})
+
 test('clicking on "Vrijwilligerswerk" in WorkWithUs navigates to the correct page', () => {
-    render(
-        <MemoryRouter initialEntries={['/']}>
-            <SideMenuWork />
-        </MemoryRouter>
-    );
-    const linkElement = screen.getByTestId('link-item-vrijwilligerswerk');
-    const anchorElement = linkElement.querySelector('a');
-    const hrefAttribute = anchorElement?.getAttribute('href');
-    expect(linkElement).toBeInTheDocument();
-    expect(hrefAttribute).toBe('/work/VolunteerWork');
-});
+  render(<SideMenuWork />)
+  const linkElement = screen.getByTestId('link-item-vrijwilligerswerk')
+  const anchorElement = linkElement.querySelector('a')
+  const hrefAttribute = anchorElement?.getAttribute('href')
+  expect(linkElement).toBeInTheDocument()
+  expect(hrefAttribute).toBe('/work/VolunteerWork')
+})
+
 test('clicking on "Job Alert" in WorkWithUs navigates to the correct page', () => {
-    render(
-        <MemoryRouter initialEntries={['/']}>
-            <SideMenuWork />
-        </MemoryRouter>
-    );
-    const linkElement = screen.getByTestId('link-item-job-alert');
-    const anchorElement = linkElement.querySelector('a');
-    const hrefAttribute = anchorElement?.getAttribute('href');
-    expect(linkElement).toBeInTheDocument();
-    expect(hrefAttribute).toBe('https://werkenbijparnassiagroep.nl/vacatures/job-alert');
-});
+  render(<SideMenuWork />)
+  const linkElement = screen.getByTestId('link-item-job-alert')
+  const anchorElement = linkElement.querySelector('a')
+  const hrefAttribute = anchorElement?.getAttribute('href')
+  expect(linkElement).toBeInTheDocument()
+  expect(hrefAttribute).toBe('https://werkenbijparnassiagroep.nl/vacatures/job-alert')
+})
+
 test('clicking on "Werken in de wijk" in WorkWithUs navigates to the correct page', () => {
-    render(
-        <MemoryRouter initialEntries={['/']}>
-            <SideMenuWork />
-        </MemoryRouter>
-    );
-    const linkElement = screen.getByTestId('link-item-werken-in-de-wijk');
-    const anchorElement = linkElement.querySelector('a');
-    const hrefAttribute = anchorElement?.getAttribute('href');
-    expect(linkElement).toBeInTheDocument();
-    expect(hrefAttribute).toBe('/work/WorkWithClients');
-});
+  render(<SideMenuWork />)
+  const linkElement = screen.getByTestId('link-item-werken-in-de-wijk')
+  const anchorElement = linkElement.querySelector('a')
+  const hrefAttribute = anchorElement?.getAttribute('href')
+  expect(linkElement).toBeInTheDocument()
+  expect(hrefAttribute).toBe('/work/WorkWithClients')
+})
+
 test('clicking on "Werken als woonbegeleider" in WorkWithUs navigates to the correct page', () => {
-    render(
-        <MemoryRouter initialEntries={['/']}>
-            <SideMenuWork />
-        </MemoryRouter>
-    );
-    const linkElement = screen.getByTestId('link-item-werken-als-woonbegeleider');
-    const anchorElement = linkElement.querySelector('a');
-    const hrefAttribute = anchorElement?.getAttribute('href');
-    expect(linkElement).toBeInTheDocument();
-    expect(hrefAttribute).toBe('/work/WorkAsResidentialSupervisor');
-});
+  render(<SideMenuWork />)
+  const linkElement = screen.getByTestId('link-item-werken-als-woonbegeleider')
+  const anchorElement = linkElement.querySelector('a')
+  const hrefAttribute = anchorElement?.getAttribute('href')
+  expect(linkElement).toBeInTheDocument()
+  expect(hrefAttribute).toBe('/work/WorkAsResidentialSupervisor')
+})
+
 test('clicking on "Werken bij Ouderpsychiatri" in WorkWithUs navigates to the correct page', () => {
-    render(
-        <MemoryRouter initialEntries={['/']}>
-            <SideMenuWork />
-        </MemoryRouter>
-    );
-    const linkElement = screen.getByTestId('link-item-werken-bij-ouderpsychiatri');
-    const anchorElement = linkElement.querySelector('a');
-    const hrefAttribute = anchorElement?.getAttribute('href');
-    expect(linkElement).toBeInTheDocument();
-    expect(hrefAttribute).toBe('/work/WorkAtGeriatricPsychiatry');
-});
+  render(<SideMenuWork />)
+  const linkElement = screen.getByTestId('link-item-werken-bij-ouderpsychiatri')
+  const anchorElement = linkElement.querySelector('a')
+  const hrefAttribute = anchorElement?.getAttribute('href')
+  expect(linkElement).toBeInTheDocument()
+  expect(hrefAttribute).toBe('/work/WorkAtGeriatricPsychiatry')
+})
--- antes/src/__test__/dashboard.test.tsx
+++ antes/src/__test__/dashboard.test.tsx
@@ -1,68 +1,58 @@
-import { render, fireEvent, screen } from '@testing-library/react';
-import { useSession } from 'next-auth/react';
-import Dashboard from '@/app/dashboard/page';
-import { ELearningCard } from '@/components/dashboard/elearning_card';
+import { render, fireEvent, screen } from '@testing-library/react'
+import { useSession } from 'next-auth/react'
+import Dashboard from '@/app/dashboard/page'
+import { ELearningCard } from '@/components/dashboard/elearning_card'
 
-// Mock next/router and next-auth/react modules
 jest.mock('next/navigation', () => ({
-    useRouter: () => ({
-        push: jest.fn(),
-        route: '/dashboard',
-        query: {},
-    }),
-}));
+  useRouter: () => ({
+    push: jest.fn(),
+    route: '/dashboard',
+    query: {},
+  }),
+}))
 
-jest.mock('next-auth/react');
+jest.mock('next-auth/react')
 
 describe('Dashboard', () => {
-    test('redirects correctly when the E-Learning button is pressed', async () => {
-        // Mocking the authenticated session
-        (useSession as jest.Mock).mockReturnValue({
-            data: { user: { id: 'clri1xxm6000a12aqak9h1fre', name: 'Ardit Fazliji', email: 'johndoe@example.com' }, expires: '2024-02-20T00:00:00.000Z' },
-            status: 'authenticated',
-        });
+  test('redirects correctly when the E-Learning button is pressed', async () => {
+    (useSession as jest.Mock).mockReturnValue({
+      data: { user: { id: 'clri1xxm6000a12aqak9h1fre', name: 'Ardit Fazliji', email: 'johndoe@example.com' }, expires: '2024-02-20T00:00:00.000Z' },
+      status: 'authenticated',
+    })
+    
+    render(<Dashboard />)
 
-        // Render the component
-        render(<Dashboard />);
+    const allButtons = screen.getByText('Ga verder')
+    fireEvent.click(allButtons)
+    const push = require('next/navigation').useRouter().push
+    expect(push).toHaveBeenCalledWith('/elearning')
+  })
 
-        const allButtons = screen.getByText('Ga verder');
-        fireEvent.click(allButtons); // Clicks the first button
-        // Checking if push was called with the expected argument
-        const push = require('next/navigation').useRouter().push;
-        expect(push).toHaveBeenCalledWith('/elearning');
-    });
-    test('redirects correctly when the Q&A button is pressed', async () => {
-        // Mocking the authenticated session
-        (useSession as jest.Mock).mockReturnValue({
-            data: { user: { id: 1, name: 'John Doe', email: 'johndoe@example.com' }, expires: '2024-02-20T00:00:00.000Z' },
-            status: 'authenticated',
-        });
+  test('redirects correctly when the Q&A button is pressed', async () => {
+    (useSession as jest.Mock).mockReturnValue({
+      data: { user: { id: 1, name: 'John Doe', email: 'johndoe@example.com' }, expires: '2024-02-20T00:00:00.000Z' },
+      status: 'authenticated',
+    })
 
-        // Render the component
-        render(<Dashboard />);
+    render(<Dashboard />)
 
-        // Simulate button click within ELearningCard component
-        const allButtons = screen.getAllByText('Ga verder');
-        fireEvent.click(allButtons[0]); // Clicks the first button
-        // Checking if push was called with the expected argument
-        const push = require('next/navigation').useRouter().push;
-        expect(push).toHaveBeenCalledWith('/qa');
-    });
-    test('redirects correctly when the event button is pressed', async () => {
-        // Mocking the authenticated session
-        (useSession as jest.Mock).mockReturnValue({
-            data: { user: { id: 1, name: 'John Doe', email: 'johndoe@example.com' }, expires: '2024-02-20T00:00:00.000Z' },
-            status: 'authenticated',
-        });
+    const allButtons = screen.getAllByText('Ga verder')
+    fireEvent.click(allButtons[0])
+    const push = require('next/navigation').useRouter().push
+    expect(push).toHaveBeenCalledWith('/qa')
+  })
+  
+  test('redirects correctly when the event button is pressed', async () => {
+    (useSession as jest.Mock).mockReturnValue({
+      data: { user: { id: 1, name: 'John Doe', email: 'johndoe@example.com' }, expires: '2024-02-20T00:00:00.000Z' },
+      status: 'authenticated',
+    })
 
-        // Render the component
-        render(<Dashboard />);
+    render(<Dashboard />)
 
-        // Simulate button click within ELearningCard component
-        const allButtons = screen.getAllByText('Ga verder');
-        fireEvent.click(allButtons[1]); // Clicks the first button
-        // Checking if push was called with the expected argument
-        const push = require('next/navigation').useRouter().push;
-        expect(push).toHaveBeenCalledWith('/event');
-    });
-});
+    const allButtons = screen.getAllByText('Ga verder')
+    fireEvent.click(allButtons[1])
+    const push = require('next/navigation').useRouter().push
+    expect(push).toHaveBeenCalledWith('/event')
+  })
+})
